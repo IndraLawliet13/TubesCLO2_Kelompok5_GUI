26ce7c6717c41595a2560b240b82df0d3d95b487
Indra Yuda <indralawliet13@gmail.com>
2025-06-12 19:59:37 +0700

    init project.

---
+++ TubesCLO2_Kelompok5_GUI/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TubesCLO2_Kelompok5_GUI.Models;
+
+namespace TubesCLO2_Kelompok5_GUI.Controllers
+{
+    public class LoginController
+    {
+        private readonly User _staticUser = new User { Username = "admin", Password = "password" };
+
+        public bool ValidateLogin(string username, string password)
+        {
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                return false;
+            }
+            return username == _staticUser.Username && password == _staticUser.Password;
+        }
+    }
+}
---
+++ TubesCLO2_Kelompok5_GUI/Models/ApiResponse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace TubesCLO2_Kelompok5_GUI.Models
+{
+    public class ApiResponse<T>
+    {
+        [JsonPropertyName("status")]
+        public int Status { get; set; }
+
+        [JsonPropertyName("message")]
+        public string? Message { get; set; }
+
+        [JsonPropertyName("data")]
+        public T? Data { get; set; }
+    }
+
+    public class ApiResponse
+    {
+        [JsonPropertyName("status")]
+        public int Status { get; set; }
+
+        [JsonPropertyName("message")]
+        public string? Message { get; set; }
+
+        [JsonPropertyName("data")]
+        public object? Data { get; set; }
+    }
+}
